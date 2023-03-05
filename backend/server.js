79795f427f418a57cbb29d3f8fb29a972327d324
Pope <andreipope16@gmail.com>
2023-03-05 19:14:37 +0200

    first commit

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,10 @@ app.use("/api/user", UserRoute);
 app.use("/api/deposit", DepositRoute);
 app.use("/api/withdraw", WithDrawRoute);
 
-const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
